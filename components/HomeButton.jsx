aaf2596ef56330b846706b2453fa9760f1b44060
Elena Kroupkin <pocoat@gmail.com>
2025-01-31 16:30:05 +1300

    update UI colors and home button

---
+++ components/HomeButton.jsx
@@ -0,0 +1,15 @@
+
+import './homebutton.css'
+const HomeButton = () => {
+  return (
+    <div className='home-container'>
+    <svg  className="homeBTN" width="20px" height="20px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fillRule="evenodd" clipRule="evenodd" d="M8 0L0 6V8H1V15H4V10H7V15H15V8H16V6L14 4.5V1H11V2.25L8 0ZM9 10H12V13H9V10Z" />
+</svg>
+    
+    
+    </div>
+  )
+}
+
+export default HomeButton
--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,3 @@
-// import { useState, useEffect } from "react";
-// import "./App.css";
-// import "./toggle.css";
-// import quizData from "./quizeData";
-// import Question from "../components/Question";
-// import StarSky from "../components/SratSky";
-// import Result from "../components/Result";
-// import ProgressBar from "../components/ProgressBar";
-// import Welcome from "../components/Welcome";
-// import ToggleButton from "../components/ToggleButton";
-
-// function App() {
-//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-//   const currentQuestion = quizData[currentQuestionIndex];
-//   const [userAnswers, setUserAnswers] = useState([]);
-//   const [isDarkMode, setIsDarkMode] = useState(false);
-//   const [mode, setMode] = useState(null); 
-//   const [isAnswerCorrect, setIsAnswerCorrect] = useState(false); 
-//   const progress = (currentQuestionIndex / quizData.length) * 100;
-
-//   useEffect(() => {
-//     if (isDarkMode) {
-//       document.body.classList.add("dark-mode");
-//     } else {
-//       document.body.classList.remove("dark-mode");
-//     }
-//   }, [isDarkMode]);
-
-//   const toggleTheme = () => {
-//     setIsDarkMode(!isDarkMode);
-//   };
-
-//   const handleNextQuestion = (selectedAnswerIndex) => {
-//     const isCorrect = selectedAnswerIndex === currentQuestion.correct;
-//     setUserAnswers((prev) => [...prev, isCorrect]);
-
-//     if (mode === "normal" || isCorrect) {
-//       setCurrentQuestionIndex((prev) => Math.min(quizData.length, prev + 1));
-//       setIsAnswerCorrect(false); 
-//     } else {
-//       setIsAnswerCorrect(true); // Indicate that the answer is correct
-//     }
-//   };
-
-//   const calculateScore = () => {
-//     return userAnswers.filter((answer) => answer).length;
-//   };
-
-//   const resetQuiz = () => {
-//     setCurrentQuestionIndex(0);
-//     setUserAnswers([]);
-//     setIsAnswerCorrect(false); // Reset answer correctness
-//     setMode(null); // Reset mode to go back to the Welcome page
-//   };
-
-//   const handleSelectMode = (selectedMode) => {
-//     setMode(selectedMode);
-//     resetQuiz(); // Reset quiz when mode changes
-//   };
-
-//   if (!mode) {
-//     return <Welcome onSelectMode={handleSelectMode} isDarkMode={isDarkMode} toggleTheme={toggleTheme} />;
-//   }
-
-//   return (
-//     <div className="wrapper">
-//       <StarSky /> {/* background stars */}
-//       {/* main quiz container */}
-//       <h2 className="title">Big O Notation Quiz</h2>
-//       <div className="quiz-container">
-//         <header className="header">
-//           <div>
-//             <img className="logo" src="../public/ekLogo.png" alt="logo" />
-//           </div>
-//           <div className="score">
-//             {mode === "normal" && `${calculateScore()} / ${quizData.length}`}
-//           </div>
-
-//           <ToggleButton isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-//         </header>
-//         <main>
-//         {currentQuestionIndex < quizData.length ? (
-//           <Question
-//             code={currentQuestion.code}
-//             language={currentQuestion.language}
-//             answers={currentQuestion.answers}
-//             onAnswerClick={handleNextQuestion}
-//             correctAnswerIndex={currentQuestion.correct}
-//           />
-//         ) : (
-//           mode === "normal" ? (
-//             <div>
-//               <Result userAnswers={userAnswers} resetQuiz={resetQuiz} />
-//               <button onClick={() => setMode(null)}>Back to Welcome</button>
-//             </div>
-//           ) : (
-//             <div className="greeting">
-//               <h2>Yey, you did it!</h2>
-//               <button onClick={resetQuiz}>Play Again</button>
-//               <button onClick={() => setMode(null)}>Back to Welcome</button>
-//             </div>
-//           )
-//         )}
-//         <footer className="footer">
-//           <ProgressBar progress={progress} />
-//         </footer>
-//         </main>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 
@@ -125,6 +12,7 @@ import Result from "../components/Result";
 import ProgressBar from "../components/ProgressBar";
 import Welcome from "../components/Welcome";
 import ToggleButton from "../components/ToggleButton";
+import HomeButton from "../components/HomeButton";
 
 
 function App() {
@@ -190,7 +78,8 @@ function App() {
       <div className="quiz-container">
         <header className="header">
           <div onClick={handleLogoClick} title="home">
-            <img className="logo" src="/ekLogo.png" alt="logo"  />
+           
+            <HomeButton />
           </div>
           <div className="score">
           {mode === "normal" && `${calculateScore()} / ${quizData.length}`}
